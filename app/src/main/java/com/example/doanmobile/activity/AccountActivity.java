b25e15eba66ffd3d626a38401a1e96b4ed7a46f5
tahn2811 <22130196@st.hcmuaf.edu.vn>
2025-06-22 01:58:05 +0700

    UI trang admin

--- app/src/main/java/com/example/doanmobile/activity/AccountActivity.java
+++ app/src/main/java/com/example/doanmobile/activity/AccountActivity.java
@@ -1,6 +1,7 @@
 package com.example.doanmobile.activity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,8 +10,9 @@ import com.example.doanmobile.R;
 public class AccountActivity extends AppCompatActivity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile); // layout tạm
-
+        setContentView(R.layout.activity_profile); // layout
+        ImageButton btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(v -> finish());
     }
 
 }
